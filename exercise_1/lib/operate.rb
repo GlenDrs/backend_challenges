aec6c0979dcdc8380a71630d1e361dd9d802c716
GlenDu <glen_llaci@yahoo.com>
2022-03-04 07:18:42 +0100

    App exc_1 level0

---
+++ exercise_1/lib/operate.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+require_relative 'read'
+
+class Operate
+
+end
---
+++ exercise_1/lib/read.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+require_relative '../data/input'
+
+class Read
+  def read_json
+  end
+
+  private
+
+  def file_json
+    './data/input.json'
+  end
+end
