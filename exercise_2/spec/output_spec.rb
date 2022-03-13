8fd61d8345a17224456b73b7640670976f76e29f
GlenDu <glen_llaci@yahoo.com>
2022-03-13 12:37:04 +0100

    tests first level

---
+++ exercise_2/spec/output_spec.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+require 'spec_helper'
+require_relative '../lib/output'
+
+RSpec.describe Output do
+  subject(:out_hash) {described_class.new }
+
+  describe '#print_hash2' do
+    it 'testing hash return, new prices'
+
+    end
+  end
+
+end
---
+++ exercise_2/spec/read_operate_spec.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+require 'spec_helper'
+require_relative '../lib/read_operate'
+
+RSpec.describe ReadOperate do
+  subject(:read) {described_class.new }
+
+  describe '#compute_array' do
+    it 'array return data test'
+
+    end
+  end
+
+end
