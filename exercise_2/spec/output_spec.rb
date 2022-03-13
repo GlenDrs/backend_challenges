6dd746b59104aaf0f257a7a50abe9f6cec060251
GlenDu <glen_llaci@yahoo.com>
2022-03-13 13:36:01 +0100

    test finished

--- exercise_2/spec/output_spec.rb
+++ exercise_2/spec/output_spec.rb
@@ -4,6 +4,7 @@ require_relative '../lib/output'
 RSpec.describe Output do
   subject(:out_hash) { described_class.new.print_hash2 }
   subject(:hash_part) { [{'id'=>1, 'price'=>3000}, {'id'=>3, 'price'=>27800}] }
+
   describe '#print_hash2' do
     it 'testing hash return, new prices' do
       expect(out_hash).to be_a Hash
