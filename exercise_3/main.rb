5e04bbc308101d4e9cd107dde3aa42021807d1d0
GlenDu <glen_llaci@yahoo.com>
2022-04-25 15:58:16 +0200

    read class test

--- exercise_3/main.rb
+++ exercise_3/main.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 require 'date'
 require_relative './lib/output'
+require_relative './lib/read'
 
   p Output.new.ouput_rentals
--- exercise_3/spec/read_spec.rb
+++ exercise_3/spec/read_spec.rb
@@ -1,2 +1,16 @@
 # frozen_string_literal: true
 require_relative '../lib/read'
+
+RSpec.describe Read do
+  subject(:read) { described_class.new }
+  subject(:hash_part) {{"id"=>3, "car_id"=>1, "start_date"=>"2015-07-3", "end_date"=>"2015-07-14", "distance"=>1000}}
+
+  describe '#call' do
+    it 'Calling Json' do
+      expect(read.call).to be_an Hash
+      expect(read.call).to include
+      hash_part
+    end
+  end
+
+end
