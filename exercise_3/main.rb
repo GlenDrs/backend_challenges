0302039e071f17477ac8a6f8edf3301fb29bba3f
GlenDu <glen_llaci@yahoo.com>
2022-04-25 21:25:10 +0200

    test operate

--- exercise_3/main.rb
+++ exercise_3/main.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 require 'date'
 require_relative './lib/output'
-require_relative './lib/read'
+require_relative './lib/operate'
 
-  p Output.new.ouput_rentals
+  p Operate.new.final_price.first.class
--- exercise_3/spec/operate_spec.rb
+++ exercise_3/spec/operate_spec.rb
@@ -6,7 +6,9 @@ RSpec.describe Operate do
 
   describe '#final_price' do
     it 'Testing the array of prices' do
-
+      expect(prices.final_price.sample).to be_an Integer
+      expect(prices.final_price).to include
+      [3000, 6800, 27800]
     end
   end
 
