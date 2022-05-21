f3cd0148a538a4be093186424901864f91cbc081
GlenDu <glen_llaci@yahoo.com>
2022-05-21 23:08:55 +0200

    new test operate_spec

--- exercise_1/spec/read_spec.rb
+++ exercise_1/spec/read_spec.rb
@@ -3,7 +3,7 @@ require_relative '../lib/read'
 
 RSpec.describe Read do
   subject(:json_doc) { described_class.new }
-  subject(:partial_hash) { [{'id'=>2, 'price_per_day'=>3000, 'price_per_km'=>15},
+  subject(:partial_hash) {:rentals=> [{'id'=>2, 'price_per_day'=>3000, 'price_per_km'=>15},
   { 'id': 3, 'price_per_day': 1700, 'price_per_km': 8 }] }
   subject(:global_hash) { { 'cars'=> [], 'rentals'=> [] } }
   describe '#read_json' do
