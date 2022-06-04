853fb52bfd26d9cd376e1c05fdd9bdb4c7851f94
GlenDu <glen_llaci@yahoo.com>
2022-06-04 19:04:14 +0200

    new tests

--- exercise_3/spec/operate_spec.rb
+++ exercise_3/spec/operate_spec.rb
@@ -1,13 +1,14 @@
 # frozen_string_literal: true
+require_relative '../lib/read'
 require_relative '../lib/operate'
 
 RSpec.describe Operate do
-  subject(:prices) { described_class.new }
+  subject(:prices) { described_class.new(Read.new.read_json_3) }
 
-  describe '#final_price' do
-    it 'Testing the array of prices' do
-      expect(prices.final_price.sample).to be_an Integer
-      expect(prices.final_price).to include
+  describe '#discount_prices_and_fees' do
+    it 'Testing discount_prices_and_fees' do
+      expect(prices.discount_prices_and_fees).to be_a Hash
+      expect(prices.discount_prices_and_fees).to include
       [3000, 6800, 27800]
     end
   end
---
+++ exercise_3/spec/rental_spec.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+require 'date'
+require_relative '../lib/rental'
+
+RSpec.describe Rental do
+  subject(:rental_object) { described_class.new(
+      id:1,
+      car_id: 1,
+      start_date: "2022-01-01",
+      end_date: "2022-02-01",
+      distance: 1
+    )}
+  subject(:discount_calcul) {1 +  0.9 * 3 + 0.7 * 6 + (32-10)*0.5}
+  describe '#initialize' do
+    it 'testing initialize of Car' do
+      expect(rental_object.id).to eq 1
+      expect(rental_object.car_id).to eq 1
+      expect(rental_object.start_date).to eq "2022-01-01"
+      expect(rental_object.end_date).to eq "2022-02-01"
+      expect(rental_object.distance).to eq 1
+    end
+  end
+
+  describe '#day_fee' do
+    it 'testing day_fee method' do
+      expect(rental_object.day_fee).to be_an Integer
+    end
+  end
+
+  describe '#discounts_days' do
+    it 'testing discounts_days method' do
+      expect(rental_object.discounts_days).to eq discount_calcul
+    end
+  end
+end
